d8b1caaf676eedf3f42436dc248fec8cd918bc98
amalkannur <amalashok.knr@gmail.com>
2024-08-29 18:13:22 +0530

    Started/Setup New Project of personal Website installed packages - Bootstrap-5 , Sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-dark w-100 d-flex justify-content-center align-items-center " style={{height: "100vh"}}  >
+      <h1 className="text-danger text-center"> Welcome To Dev of Personal Website  <br /> - Amal Ashok </h1>
     </div>
   );
 }
